10eefb0c172ba74df2719a695d6c7353176e8db2
jiyu <wkdduq4840@gmail.com>
2023-05-23 23:27:14 +0900

    add users controller"

---
+++ src/users/users.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Body, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('users')
+export class UsersController {
+    @Post()
+    async createUser(@Body() dto: CreateUserDto): Promise<void> {
+        console.log(dto);
+    }
+}
