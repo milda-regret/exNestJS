c70f8dfd9d8a8b18e86c3710578a6288b0b4ef7c
jiyu <wkdduq4840@gmail.com>
2023-05-23 23:35:08 +0900

    add verify email

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Body, Post } from '@nestjs/common';
+import { Controller, Body, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
 
 @Controller('users')
 export class UsersController {
     @Post()
     async createUser(@Body() dto: CreateUserDto): Promise<void> {
         console.log(dto);
+    }
+
+    @Post('/email-verify')
+    async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+        console.log(dto);
+        return;
     }
 }
